b5a7ad9a3a18d374407a405293bc1453f8f0d536
yunuska01 <121348895+yunuska01@users.noreply.github.com>
2022-12-24 19:49:50 +0500

    Create memory_card.py
    version 1.0

---
+++ memory_card.py
@@ -0,0 +1,54 @@
+class Wall(sprite.Sprite):
+    def init(self,color_1,color_2,color_3,x,y,width,height):
+        super().init()
+        self.color_1 = color_1
+        self.color_2 = color_2
+        self.color_3 = color_3
+        self.width = width
+        self.height=height
+        self.image=Surface((self.width,self.height))
+        self.image.fill((color_1,color_2,color_3))
+        self.rect= self.image.get_rect()
+        self.rect.x= x
+        self.rect.y = y
+    def drawall(self):
+        window.blit(self.image,(self.rect.x,self.rect.y))
+wall_1 = Wall(41,23,25,200,250,40,400)
+wall_2 = Wall(41,23,25,320,20,20,350)
+wall_3 = Wall(41,23,25,450,200,10,450)
+
+winText = font.render('йу вин', True, (0,0,0))
+loseText = font.render('йу лусер', True, (0,0,0))
+hero = Player("hero.png",5,400,5)
+cyborg = Enemy('cyborg.png',600,300,3)
+treasure = GameSprite('treasure.png',600,400,0)
+mixer.music.load('jungles.ogg')
+mixer.music.play()
+win_music = mixer.Sound('kick.ogg')
+lose_music = mixer.Sound('money.ogg')
+finish = False
+clock = time.Clock()
+game = True
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    if finish != True:
+        if sprite.collide_rect(hero,treasure):
+            finish = True
+            lose_music.play()
+        if sprite.collide_rect(hero,cyborg) or sprite.collide_rect(hero,wall_1) or sprite.collide_rect(hero,wall_2) or sprite.collide_rect(hero,wall_3):
+            finish = True
+            window.blit(winText,(200,200))
+            win_music.play()
+        window.blit(background,(0,0))
+        hero.update()
+        hero.reset()
+        cyborg.auto()
+        cyborg.reset()
+        wall_1.drawall()
+        wall_2.drawall()
+        wall_3.drawall()
+        treasure.reset()
+        display.update()
+        clock.tick(60)
